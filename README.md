cef77f12adbbc03352167214e2440fe6c7a9bf8e
Sam Hogg <samhogg@me.com>
2013-11-05 14:56:05 +1300

    Added a README. Yo.

---
+++ README.md
@@ -0,0 +1,5 @@
+# milehigh-node
+
+A simple node.js skeleton for connecting to [Hacktivate](http://hacktivate.me)'s MILE HIGH: CHCH competition.
+
+Created by [@samhogg](http://samhogg.com)
---
+++ mileHigh.js
@@ -0,0 +1,70 @@
+var CONNECTION_URL = "http://challenge.hacktivate.me:3000",
+    request = require('request'),
+    async = require('async'),
+    spawn = require('child_process').spawn;
+
+var mileHigh = function(token) {
+    async.forever(
+        function (callback) {
+            request(CONNECTION_URL +"/get?token=" + token, function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+
+                // Parse the response body
+                var getData = JSON.parse(body);
+
+                // You might want to put something in here to send to the server ;)
+                var postData = {};
+
+                // For now, we'll just log the response
+                console.log(body);
+
+                // Sending our data to the server
+                request.post(CONNECTION_URL +"/post", {json: postData});
+
+              } else {
+
+                // Ooopsie
+                console.log("ERROR: " + error);
+
+              }
+            });
+
+            // By all means, tweak this timeout. No guarantees on how fast your requests will be though :)
+            setTimeout(callback, 50);
+        },
+
+        // An error happened somewhere in async.forever
+        function (err) {
+            console.log("There was an error. Oops. Here it is: " + err);
+        }
+    );
+}
+
+
+var initialiseMileHigh = function() {
+
+    // If we give mileHigh a token, it will connect to that
+    if(process.argv[2]) {
+        //Mac OS Specific. Feel free to remove if not helpful for you :)
+        spawn('open', [CONNECTION_URL + "/view3d?token=" + process.argv[2]]);
+        mileHigh(process.argv[2]);
+    
+    // Otherwise, create a new 'practice' session
+    } else {
+        request(CONNECTION_URL +"/new-session", function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            
+            var token = JSON.parse(body).token;
+
+            //Mac OS Specific. Feel free to remove if not helpful for you :)
+            spawn('open', [CONNECTION_URL + "/view3d?token=" +token]);
+            mileHigh(token);
+          } else {
+            console.log("ERROR: " + error);
+          }
+        });
+    }
+};
+
+
+var mileHighInstance = new initialiseMileHigh();
